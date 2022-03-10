e9d480dad7a9b44e6557d91a582a9db0b9c33f80
shanmugasundran1 <shanmugasundran1@gmail.com>
2022-03-11 00:05:24 +0800

    Initial Push

---
+++ app/src/main/java/com/example/idiary/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.idiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
